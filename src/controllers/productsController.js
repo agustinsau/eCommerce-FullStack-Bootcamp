e6d1dd888c6cb50b9f9c88412dce07d54907f0a3
Agustin Sau <sau.agustin.i@gmail.com>
2024-05-08 17:43:08 -0300

    creado producto en la vista del home

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -2,16 +2,16 @@ const path = require('path');
 
 const db = require('../database/models');
 
-const Product = db.Product;
-const Genre = db.Genre;
-const Artist = db.Artist;
+const Products = db.Product;
+const Genres = db.Genre;
+const Artists = db.Artist;
 
 const productsController = {
     showAll: (req, res) => {
         Promise.all([
-            Product.findAll(),
-            Genre.findAll(),
-            Artist.findAll()
+            Products.findAll(),
+            Genres.findAll(),
+            Artists.findAll()
         ])
         .then(([products, genres, artists]) => {
             res.render(path.resolve(__dirname, '..', 'views', 'index'), { products, genres, artists });
