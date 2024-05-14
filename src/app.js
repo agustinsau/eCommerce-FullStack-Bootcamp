7eb6c904b69133016c47a36f0c2e43dddca4c136
Agustin Sau <sau.agustin.i@gmail.com>
2024-05-14 19:35:58 -0300

    arreglado bootstrap en clase de consulta

--- src/app.js
+++ src/app.js
@@ -3,7 +3,8 @@
 const express =  require('express');
 const path = require("path");
 const app = express();
-const methodOverride  = require("method-override");
+const methodOverride = require("method-override");
+//const btstrap = require("bootstrap");
 
 //Routes
 const indexRouter = require('./routes/index');
@@ -13,8 +14,15 @@ const productsRouter = require('./routes/productsRoutes');
 app.set( "view engine", "ejs" );
 app.set('views', path.resolve(__dirname, './views'));
 
-//ruta recursos estaticos
+// Configurar la carpeta 'public' para servir archivos estáticos
 app.use(express.static(path.join(__dirname, '../public')));
+
+// Configurar rutas para Bootstrap CSS y JS desde 'node_modules'
+// Sirve los archivos CSS de Bootstrap
+//app.use('/css', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/css')));
+
+// Sirve los archivos JavaScript de Bootstrap
+//app.use('/js', express.static(path.join(__dirname, 'node_modules/bootstrap/dist/js')));
 
 app.use(express.urlencoded({extended:false}));
 app.use(methodOverride("_method"));
@@ -23,9 +31,7 @@ app.listen(3001 , ()=>{
     console.log("Server is running on port 3001")
 });
 
-
 //Uso de rutas
-
 //ruta raiz
 app.use('/', indexRouter);
 
